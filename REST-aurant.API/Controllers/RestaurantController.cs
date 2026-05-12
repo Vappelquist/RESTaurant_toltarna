4b848aeb13bb3b2cfc8fea7e2e394d69bba12297
RobinJohansson1992 <robin-johansson@live.com>
2026-05-12 15:06:51 +0200

    created controllers

---
+++ REST-aurant.API/Controllers/RestaurantController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using REST_aurant.API.Data;
+using Restaurant.Models.Models;
+
+namespace REST_aurant.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+        
+    }
+}
