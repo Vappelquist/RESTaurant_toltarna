cd54cd49fba23f9510811dd29585a0344bae9830
Sara Andersson <paphatsara1995@hotmail.com>
2026-05-13 20:06:49 +0200

    Added GetAllBookings endpoint with response DTO

--- REST-aurant.API/Controllers/BookingController.cs
+++ REST-aurant.API/Controllers/BookingController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using REST_aurant.API.Data;
+using static REST_aurant.API.DTOs.Booking;
 
 namespace REST_aurant.API.Controllers
 {
@@ -7,5 +10,37 @@ namespace REST_aurant.API.Controllers
     [ApiController]
     public class BookingController : ControllerBase
     {
+        private readonly RestaurantDbContext _ctx;
+
+        public BookingController(RestaurantDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        //Get: AllBookings
+        [HttpGet(Name = "GetAllBookings")]
+        public async Task<ActionResult<IEnumerable<GetAllBookingResponse>>> GetAllBookings()
+        {
+            var bookings = await _ctx.Bookings
+                .AsNoTracking()
+                .Select(b => new GetAllBookingResponse
+                {
+                    BookingId = b.Id,
+                    GuestName = $"{b.Guest.FirstName} {b.Guest.LastName}",
+                    AmountOfGuests = b.AmountOfGuests,
+                    Status = b.Status,
+                    DateBooked = DateOnly.FromDateTime(b.DateBooked),
+                    StartTime = b.StartTime.ToString("yyyy-MM-dd HH:mm"),
+                    EndTime = b.EndTime.ToString("yyyy-MM-dd HH:mm"),
+                    BookingNotes = b.BookingNotes,
+                    TableNumbers = b.Tables.Select(t => t.TableNumber).ToList()
+                })
+                .ToListAsync();
+            if(!bookings.Any())
+            {
+                return NotFound("Det finns ingen bokning");
+            }
+         
+            return Ok(bookings);
+        }
     }
 }
