9a24471c7da27eac49a1d6533336d8c63f163b61
Sara Andersson <paphatsara1995@hotmail.com>
2026-05-14 13:49:16 +0200

    Created GET weekly bookings by week

--- REST-aurant.API/Controllers/BookingController.cs
+++ REST-aurant.API/Controllers/BookingController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using REST_aurant.API.Data;
 using static REST_aurant.API.DTOs.Booking;
+using System.Globalization;
 
 namespace REST_aurant.API.Controllers
 {
@@ -29,17 +30,53 @@ namespace REST_aurant.API.Controllers
                     AmountOfGuests = b.AmountOfGuests,
                     Status = b.Status,
                     DateBooked = DateOnly.FromDateTime(b.DateBooked),
-                    StartTime = b.StartTime.ToString("yyyy-MM-dd HH:mm"),
-                    EndTime = b.EndTime.ToString("yyyy-MM-dd HH:mm"),
+                    StartTime = b.StartTime,
+                    EndTime = b.EndTime,
                     BookingNotes = b.BookingNotes,
                     TableNumbers = b.Tables.Select(t => t.TableNumber).ToList()
                 })
                 .ToListAsync();
             if(!bookings.Any())
             {
-                return NotFound("Det finns ingen bokning");
+                return NotFound("No booking");
             }
          
+            return Ok(bookings);
+        }
+
+        //Get: weekly bookings by week
+        [HttpGet("GetWeeklyBookings")]
+        public async Task<ActionResult<IEnumerable<GetAllBookingResponse>>> GetWeeklyBookings(int week)
+        {
+            //Get current year
+            var year = DateTime.Now.Year;
+
+            var startOfWeek = ISOWeek.ToDateTime(year, week, DayOfWeek.Monday);
+            //The week starts at Monday
+            var endOfWeek = startOfWeek.AddDays(7);
+
+            var bookings = await _ctx.Bookings
+                .AsNoTracking()
+                .Where(b => b.StartTime >= startOfWeek && b.StartTime < endOfWeek)
+                .Select(b => new GetAllBookingResponse
+                {
+                    BookingId = b.Id,
+                    GuestName = $"{b.Guest.FirstName} {b.Guest.LastName}",
+                    AmountOfGuests = b.AmountOfGuests,
+                    Status = b.Status,
+                    DateBooked = DateOnly.FromDateTime(b.DateBooked),
+                    StartTime = b.StartTime,
+                    EndTime = b.EndTime,
+                    BookingNotes = b.BookingNotes,
+                    TableNumbers = b.Tables.Select(t => t.TableNumber).ToList()
+                })
+                .ToListAsync();
+
+            if(!bookings.Any())
+            {
+                return NotFound("No booking this week");
+            }
+
             return Ok(bookings);
         }
     }
