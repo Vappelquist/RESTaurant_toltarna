23b01c3935549119644ad77fe81f8a85d83eff8c
Sara Andersson <paphatsara1995@hotmail.com>
2026-05-15 16:24:43 +0200

    Created Place booking for guest include admin so they can place new booking.

--- REST-aurant.API/Migrations/20260515141850_makePasswordNullable.Designer.cs
+++ REST-aurant.API/Migrations/20260515141850_makePasswordNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using REST_aurant.API.Data;
 
@@ -11,9 +12,11 @@ using REST_aurant.API.Data;
 namespace REST_aurant.API.Migrations
 {
     [DbContext(typeof(RestaurantDbContext))]
-    partial class RestaurantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260515141850_makePasswordNullable")]
+    partial class makePasswordNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
