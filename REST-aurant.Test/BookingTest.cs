219e2097557b2acc5d63a42600d0a822903a495b
RobinJohansson1992 <robin-johansson@live.com>
2026-05-19 09:32:25 +0200

    Added class BookingTest

---
+++ REST-aurant.Test/BookingTest.cs
@@ -0,0 +1,20 @@
+using Restaurant.Models.Models;
+
+namespace REST_aurant.Test;
+
+[TestClass]
+public class BookingTest
+{
+
+    private Booking _booking;
+
+    public BookingTest()
+    {
+        _booking = new Booking();
+    }
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
