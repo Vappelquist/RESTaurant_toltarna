9ff0ca1c400fa0c7f0e063348e64b992c333a2cd
RobinJohansson1992 <robin-johansson@live.com>
2026-05-21 10:12:33 +0200

    added test-method PlaceBooking_IfEndTimeIsTwoHoursAfterStartTime_ReturnTrue

--- REST-aurant.Test/BookingControllerTests.cs
+++ REST-aurant.Test/BookingControllerTests.cs
@@ -4,6 +4,7 @@ using Moq;
 using Restaurant.API.Controllers;
 using Restaurant.API.Data;
 using Restaurant.API.Services;
+using Restaurant.Models.Models;
 using static Restaurant.API.DTOs.Booking;
 
 namespace Restaurant.Test;
@@ -120,6 +121,38 @@ public class BookingControllerTests
 
         // Assert
         Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+    }
+
+    [TestMethod]
+    public async Task PlaceBooking_IfEndTimeIsTwoHoursAfterStartTime_ReturnTrue()
+    {
+        // Arrange
+        var ctx = CreateInMemoryDb();
+
+        var mockTableService = new Mock<ITableService>();
+        mockTableService
+        .Setup(s => s.AllocateTablesAsync(It.IsAny<DateTime>(), It.IsAny<DateTime>(), It.IsAny<int>(), It.IsAny<int?>())) // It.IsAny<T>() = Mock-specific, dont worry about what value it is, always match.
+        .ReturnsAsync(new List<Table> { new Table { TableNumber = 1, Seats = 4 } });
+
+        var service = new BookingService(ctx, mockTableService.Object);
+
+        var request = new PlaceBookingRequest
+        {
+            FirstName = "Sven",
+            LastName = "Svensson",
+            Email = "sven@mail.com",
+            AmountOfGuests = 2,
+            StartTime = "18:00",
+            BookingDate = DateOnly.FromDateTime(DateTime.Now.AddDays(1))
+        };
+
+        // Act
+        await service.PlaceBookingAsync(request);
+
+        // Assert
+        var booking = await ctx.Bookings.FirstOrDefaultAsync();
+        Assert.IsNotNull(booking);
+        Assert.AreEqual(booking.EndTime, booking.StartTime.AddHours(2));
     }
 
     // PlaceBooking-tests ---------------------------------------------------------------^
