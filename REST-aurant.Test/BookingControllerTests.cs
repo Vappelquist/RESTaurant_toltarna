c151ca10bd64e570e907e1e9e1dc593ab1109435
RobinJohansson1992 <robin-johansson@live.com>
2026-05-28 10:31:07 +0200

    moved PlaceBooking_IfEndTimeIsTwoHoursAfterStartTime_ReturnTrue from BookingControllerTests to BookingServiceTests

--- REST-aurant.Test/BookingControllerTests.cs
+++ REST-aurant.Test/BookingControllerTests.cs
@@ -13,14 +13,7 @@ namespace Restaurant.Test;
 [TestClass]
 public class BookingControllerTests
 {
-    // For in-memory-database:
-    private RestaurantDbContext CreateInMemoryDb()
-    {
-        var options = new DbContextOptionsBuilder<RestaurantDbContext>()
-            .UseInMemoryDatabase(Guid.NewGuid().ToString())
-            .Options;
-        return new RestaurantDbContext(options);
-    }
+    
 
     // PlaceBooking-tests ---------------------------------------------------------------V
     [TestMethod]
@@ -120,37 +113,7 @@ public class BookingControllerTests
         Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
     }
 
-    [TestMethod]
-    public async Task PlaceBooking_IfEndTimeIsTwoHoursAfterStartTime_ReturnTrue()
-    {
-        // Arrange
-        var ctx = CreateInMemoryDb();
-
-        var mockTableService = new Mock<ITableService>();
-        mockTableService
-        .Setup(s => s.AllocateTablesAsync(It.IsAny<DateTime>(), It.IsAny<DateTime>(), It.IsAny<int>(), It.IsAny<int?>())) // It.IsAny<T>() = Mock-specific, dont worry about what value it is, always match.
-        .ReturnsAsync(new List<Table> { new Table { TableNumber = 1, Seats = 4 } });
-
-        var service = new BookingService(ctx, mockTableService.Object);
-
-        var request = new PlaceBookingRequest
-        {
-            FirstName = "Sven",
-            LastName = "Svensson",
-            Email = "sven@mail.com",
-            AmountOfGuests = 2,
-            StartTime = "18:00",
-            BookingDate = DateOnly.FromDateTime(DateTime.Now.AddDays(1))
-        };
-
-        // Act
-        await service.PlaceBookingAsync(request);
-
-        // Assert
-        var booking = await ctx.Bookings.FirstOrDefaultAsync();
-        Assert.IsNotNull(booking);
-        Assert.AreEqual(booking.EndTime, booking.StartTime.AddHours(2));
-    }
+    
 
     
 
--- REST-aurant.Test/BookingServiceTests.cs
+++ REST-aurant.Test/BookingServiceTests.cs
@@ -1,10 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Restaurant.API.Data;
+using Restaurant.API.Services;
+using Restaurant.Models.Models;
+using static Restaurant.API.DTOs.Booking;
+
 namespace Restaurant.Test;
 
 [TestClass]
 public class BookingServiceTests
 {
-    [TestMethod]
-    public void TestMethod1()
+    // For in-memory-database:
+    private RestaurantDbContext CreateInMemoryDb()
     {
+        var options = new DbContextOptionsBuilder<RestaurantDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+        return new RestaurantDbContext(options);
+    }
+    [TestMethod]
+    public async Task PlaceBooking_IfEndTimeIsTwoHoursAfterStartTime_ReturnTrue()
+    {
+        // Arrange
+        var ctx = CreateInMemoryDb();
+
+        var mockTableService = new Mock<ITableService>();
+        mockTableService
+        .Setup(s => s.AllocateTablesAsync(It.IsAny<DateTime>(), It.IsAny<DateTime>(), It.IsAny<int>(), It.IsAny<int?>())) // It.IsAny<T>() = Mock-specific, dont worry about what value it is, always match.
+        .ReturnsAsync(new List<Table> { new Table { TableNumber = 1, Seats = 4 } });
+
+        var service = new BookingService(ctx, mockTableService.Object);
+
+        var request = new PlaceBookingRequest
+        {
+            FirstName = "Sven",
+            LastName = "Svensson",
+            Email = "sven@mail.com",
+            AmountOfGuests = 2,
+            StartTime = "18:00",
+            BookingDate = DateOnly.FromDateTime(DateTime.Now.AddDays(1))
+        };
+
+        // Act
+        await service.PlaceBookingAsync(request);
+
+        // Assert
+        var booking = await ctx.Bookings.FirstOrDefaultAsync();
+        Assert.IsNotNull(booking);
+        Assert.AreEqual(booking.EndTime, booking.StartTime.AddHours(2));
     }
 }
