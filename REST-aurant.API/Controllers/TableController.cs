64c079d88039473677af26947cb6b4dd33c6b9b2
RobinJohansson1992 <robin-johansson@live.com>
2026-06-01 14:00:47 +0200

    Added TableController

---
+++ REST-aurant.API/Controllers/TableController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurant.API.Services;
+
+namespace Restaurant.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TableController : ControllerBase
+    {
+        private readonly ITableService _tableService;
+
+        public TableController(ITableService tableService)
+        {
+            _tableService = tableService;
+        }
+
+    }
+}
