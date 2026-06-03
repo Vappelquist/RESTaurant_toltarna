ff8780e0d1c4ca1c7892c54b7f3a9e545de67ff4
Vilgot <vilgot.appelquist@hotmail.com>
2026-06-03 12:25:00 +0200

    Added so the GuestDTOs inherit from eachother for less repeated code

--- REST-aurant.API/DTOs/GuestDTOs.cs
+++ REST-aurant.API/DTOs/GuestDTOs.cs
@@ -5,54 +5,47 @@ namespace Restaurant.API.DTOs
 {
     public class GuestDTOs
     {
-        public record CreateAddGuestRequest
+        public record GuestBaseDTO
         {
+
             [Required]
             [MinLength(2)]
             public string? FirstName { get; init; }
+
             [Required]
             [MinLength(2)]
             public string? LastName { get; init; }
+
             [Required]
             [EmailAddress]
             public string? Email { get; init; }
+
+            [Phone]
+            public string? PhoneNumber { get; init; }
+
+            public string? Allergies { get; init; }
+
+            public string? Note { get; init; }
+
+
+        }
+        public record CreateAddGuestRequest : GuestBaseDTO
+        {
+
             [Required]
             [MinLength(8)]
-
             public string? Password { get; init; }
-            [Phone]
-            public string? PhoneNumber { get; init; }
-            public string? Allergies { get; init; }
-            public string? Note { get; init; }
+
         }
 
-        public record UpdateGuestRequest
+        public record UpdateGuestRequest : GuestBaseDTO
         {
-            [MinLength(2)]
-            public string? FirstName { get; init; }
 
-            [MinLength(2)]
-            public string? LastName { get; init; }
-
-            [EmailAddress]
-            public string? Email { get; init; }
-
-            [Phone]
-            public string? PhoneNumber { get; init; }
-
-            public string? Allergies { get; init; }
-            public string? Note { get; init; }
         }
 
-        public record GetGuestResponse
+        public record GetGuestResponse : GuestBaseDTO
         {
             public int Id { get; init; }
-            public string? FirstName { get; init; }
-            public string? LastName { get; init; }
-            public string? Email { get; init; }
-            public string? PhoneNumber { get; init; }
-            public string? Allergies { get; init; }
-            public string? Note { get; init; }
             public List<BookingStatus> BookingStatuses { get; init; } = new();
         }
     }
