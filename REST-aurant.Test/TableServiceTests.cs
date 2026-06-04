9a08eae978f25fd2c28b3d106fc4ba59beff91aa
Vilgot <vilgot.appelquist@hotmail.com>
2026-06-04 17:54:25 +0200

    Added GetAvailableTablesAsync Tests

--- REST-aurant.Test/TableServiceTests.cs
+++ REST-aurant.Test/TableServiceTests.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Restaurant.API.Data;
 using Restaurant.API.Services;
+using Restaurant.Models.Models;
+using Restaurant.Models.Models.Enums;
 
 namespace Restaurant.Test;
 
@@ -23,7 +25,7 @@ public class TableServiceTests
         using var ctx = CreateInMemoryDb();
         var services = new TableService(ctx);
 
-        ctx.Tables.Add(new Models.Models.Table
+        ctx.Tables.Add(new Table
         {
             TableNumber = 99,
             Seats = 4
@@ -56,7 +58,7 @@ public class TableServiceTests
         // Arrange
         using var ctx = CreateInMemoryDb();
         var services = new TableService(ctx);
-        ctx.Tables.Add(new Models.Models.Table
+        ctx.Tables.Add(new Table
         {
             TableNumber = 99,
             Seats = 4
@@ -75,7 +77,7 @@ public class TableServiceTests
         // Arrange
         using var ctx = CreateInMemoryDb();
         var services = new TableService(ctx);
-        ctx.Tables.Add(new Models.Models.Table
+        ctx.Tables.Add(new Table
         {
             TableNumber = 99,
             Seats = 4
@@ -94,7 +96,7 @@ public class TableServiceTests
         // Arrange
         using var ctx = CreateInMemoryDb();
         var services = new TableService(ctx);
-        ctx.Tables.Add(new Models.Models.Table
+        ctx.Tables.Add(new Table
         {
             TableNumber = 99,
             Seats = 4
@@ -126,7 +128,7 @@ public class TableServiceTests
         // Arrange
         using var ctx = CreateInMemoryDb();
         var services = new TableService(ctx);
-        ctx.Tables.Add(new Models.Models.Table
+        ctx.Tables.Add(new Table
         {
             TableNumber = 99,
             Seats = 4
@@ -147,9 +149,9 @@ public class TableServiceTests
         using var ctx = CreateInMemoryDb();
         var service = new TableService(ctx);
         ctx.Tables.AddRange(
-            new Models.Models.Table { TableNumber = 1, Seats = 4 },
-            new Models.Models.Table { TableNumber = 2, Seats = 6 },
-            new Models.Models.Table { TableNumber = 3, Seats = 2 }
+            new Table { TableNumber = 1, Seats = 4 },
+            new Table { TableNumber = 2, Seats = 6 },
+            new Table { TableNumber = 3, Seats = 2 }
             );
         await ctx.SaveChangesAsync();
 
@@ -183,7 +185,7 @@ public class TableServiceTests
         // Arrange
         using var ctx = CreateInMemoryDb();
         var service = new TableService(ctx);
-        ctx.Tables.Add(new Models.Models.Table { TableNumber = 1, Seats = 4 });
+        ctx.Tables.Add(new Table { TableNumber = 1, Seats = 4 });
         await ctx.SaveChangesAsync();
         
         //act
@@ -198,7 +200,7 @@ public class TableServiceTests
         // Arrange
         using var ctx = CreateInMemoryDb();
         var service = new TableService(ctx);
-        ctx.Tables.AddRange(new Models.Models.Table { TableNumber = 5, Seats = 8 });
+        ctx.Tables.AddRange(new Table { TableNumber = 5, Seats = 8 });
         await ctx.SaveChangesAsync();
         //act
         var result = await service.GetAllTablesAsync();
@@ -207,4 +209,81 @@ public class TableServiceTests
         Assert.AreEqual(8, result[0].Seats);
         
     }
+
+    //--GetAvailavleTablesAsync Tests--
+    [TestMethod]
+    public async Task GetAvailableTablesAsync_WithNoOverlappings_ReturnsAllTables()
+    {
+        // Arrange
+        using var ctx = CreateInMemoryDb();
+        var service = new TableService(ctx);
+        ctx.Tables.AddRange(
+            new Table { TableNumber = 1, Seats = 4 },
+            new Table { TableNumber = 2, Seats = 6 }
+            );
+        await ctx.SaveChangesAsync();
+        var start = DateTime.Now;
+        var end = DateTime.Now.AddHours(2);
+        //act
+        var result = await service.GetAvailableTablesAsync(start, end);
+        //Assert
+        Assert.HasCount(2, result);
+    }
+
+    [TestMethod]
+    public async Task GetAvailableTablesAsync_WithFullyOverlappingBooking_ReturnsOnlyUnbookedTables()
+    {
+        //Arrange
+        using var ctx = CreateInMemoryDb();
+        var service = new TableService(ctx);
+        var table1 = new Table { TableNumber = 1, Seats = 4 };
+        var table2 = new Table { TableNumber = 2, Seats = 6 };
+        ctx.Tables.AddRange(table1, table2);
+
+        var start = DateTime.Now;
+        var end = DateTime.Now.AddHours(2);
+
+        ctx.Bookings.Add(new Booking
+        {
+            StartTime = start,
+            EndTime = end,
+            Status = Models.Models.Enums.BookingStatus.Confirmed,
+            Tables = new List<Table> { table1 }
+        });
+        await ctx.SaveChangesAsync();
+
+        //Act
+        var result = await service.GetAvailableTablesAsync(start, end);
+
+        //Assert
+        Assert.HasCount(1, result);
+        Assert.AreEqual(2, result[0].TableNumber);
+    }
+    [TestMethod]
+    public async Task GetAvailableTablesAsync_WithCancelledBooking_ReturnsTableAsAvailable()
+    {
+        //Arrange
+        using var ctx = CreateInMemoryDb();
+        var services = new TableService(ctx);
+        var table1 = new Table { TableNumber = 1, Seats = 4 };
+        ctx.Tables.Add(table1);
+
+        var start = DateTime.Now;
+        var end = start.AddHours(2);
+
+        ctx.Bookings.Add(new Booking
+        {
+            StartTime = start,
+            EndTime = end,
+            Status = BookingStatus.Canceled,
+            Tables = new List<Table> { table1 }
+        });
+        await ctx.SaveChangesAsync();
+
+        //Act
+        var result = await services.GetAvailableTablesAsync(start, end);
+
+        //Assert
+        Assert.AreEqual(1, result.Count);
+    } 
 }
